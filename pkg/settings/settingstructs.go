cab07617570b620456dc09748313ee91286b4a57
Luke Maple <luke.maple@precisionplanting.com>
2024-08-23 10:04:21 -0500

    added SQL server and database settings to the toml file so that they can be managed byu the game server rather then needing to be manually setup

--- pkg/settings/settingstructs.go
+++ pkg/settings/settingstructs.go
@@ -3,15 +3,16 @@ package settings
 import "time"
 
 type Config struct {
-	Log    LogSettings    `toml:"Log Settings"`
-	Server ServerSettings `toml:"Server Settings"`
+	Log        LogSettings                  `toml:"Log Settings"`
+	Server     ServerSettings               `toml:"Game Server"`
+	SQLServers map[string]SQLServerSettings `toml:"Database Servers"`
 }
 
 type LogSettings struct {
-	MaxFileSize int64  `toml:"LogMaxSize"` // in megabytes
-	PollRate    int    `toml:"LogPollRate"`
-	FileName    string `toml:"LogFileName"`
-	Path        string `toml:"LogPath"`
+	MaxFileSize int64  `toml:"Max_Size"` // in megabytes
+	PollRate    int    `toml:"Poll_Rate"`
+	FileName    string `toml:"Log File Name"`
+	Path        string `toml:"Path"`
 }
 
 func (ls LogSettings) AdjustedPollRate() time.Duration {
@@ -22,4 +23,24 @@ type ServerSettings struct {
 	Addr        string `toml:"Address"`
 	Port        int    `toml:"Port"`
 	IdleTimeout int    `toml:"Idle_Timeout"`
+}
+
+type SQLServerSettings struct {
+	Addr      string
+	Port      int
+	Username  string
+	Password  string
+	DefaultDB string
+	Views     map[string][]SQLColumn
+	Tables    map[string][]SQLColumn
+}
+
+type SQLColumn struct {
+	Name          string  `toml:"Name"`
+	ColType       string  `toml:"Type"`
+	AutoIncrement bool    `toml:"Auto Increment"`
+	PrimaryKey    bool    `toml:"Primary Key"`
+	ForiegnKey    bool    `toml:"Foriegn Key"`
+	Nullable      bool    `toml:"Null Allowed"`
+	DefaultValue  *string `toml:"Default"`
 }
