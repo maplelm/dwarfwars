a4a6aecb47c561201e3fbecfa862c5212d4cdd6e
Luke Maple <luke.maple@precisionplanting.com>
2024-10-18 08:19:36 -0500

    removed binary files and added a run script for the client
     Changes to be committed:
    	deleted:    bt
    	deleted:    client
    	new file:   runclient.sh

---
+++ runclient.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+error() {
+	echo $1
+	exit 1
+}
+
+go build -o $PWD/bin/client/gclient ./cmd/client/ || error "Failed to build client"
+cd $PWD/bin/client/ || error "Failed to switch to client bin directory"
+./gclient
+cd -
+
