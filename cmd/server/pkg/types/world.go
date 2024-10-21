6345213b6e83268638a6d4bf878659f71458fcc1
Luke Maple <maplelm@gmail.com>
2024-10-20 21:35:52 -0500

    changing how commands work so they can be faster by not copying so much slice data, because of this commands do not marshal into binary but have a send function instead

--- cmd/server/pkg/types/world.go
+++ cmd/server/pkg/types/world.go
@@ -1,4 +1,19 @@
 package types
 
 type World struct {
+	Chunks []Chunk
 }
+
+type Chunk struct {
+	Tiles []Tile
+}
+
+type Entity interface {
+}
+
+type Tile struct {
+	Type     TileType
+	Contains []Entity
+}
+
+type TileType uint16
--- pkg/command/command.go
+++ pkg/command/command.go
@@ -2,64 +2,93 @@ package command
 
 /*
 	I need to get this encoded in BigEndian and make it just make more sense but it works pretty well right now.
+
+
+	HEADER:
+
+	Byte 0  : Version
+	byte 1-4: Client ID
+	Byte 5  : Command Type
+	Byte 6  : Data Format
+	Byte 7-10: Size
 */
 import (
 	"fmt"
 	"math"
+	"net"
 )
 
 const CurrentVersion CommandVersion = 1
 
-const HeaderSize int = 6 // bytes
+const HeaderSize int = 11 // bytes
 
 type CommandType uint8
 type CommandVersion uint8
 
 type Command struct {
-	Version CommandVersion // 8 bits
-	Type    CommandType    // 8 bits
-	Size    uint32         // 16 bits (max command size: 65_536 bytes)
-	Data    []byte
+	Version  CommandVersion // 1 Byte
+	ClientID uint32         // 4 Bytes
+	Type     CommandType    // 1 Byte
+	Format   uint8          // 1 Byte
+	Size     uint32         // 4 Bytes
+	Data     []byte
 }
 
-func New(t CommandType, d []byte) (*Command, error) {
+func New(id *uint32, format uint8, t CommandType, d []byte) (*Command, error) {
 	if len(d) > int(math.Pow(2, 32)) {
 		return nil, fmt.Errorf("command exceeds data limit")
 	}
 	return &Command{
 		Version: CurrentVersion,
-		Type:    t,
-		Size:    uint32(len(d)),
-		Data:    d,
+		ClientID: func() uint32 {
+			if id == nil {
+				return 0
+			}
+			return *id
+		}(),
+		Type:   t,
+		Format: format,
+		Size:   uint32(len(d)),
+		Data:   d,
 	}, nil
 }
 
-func (c Command) Marshal() []byte {
+func (c Command) Send(conn net.Conn) (int64, error) {
 	bytes := make([]byte, HeaderSize)
 	bytes[0] = byte(c.Version)
-	bytes[1] = byte(c.Type)
-	bytes[2] = byte((c.Size & 0xFF000000) >> 24)
-	bytes[3] = byte((c.Size & 0x00FF0000) >> 16)
-	bytes[4] = byte((c.Size & 0x0000FF00) >> 8)
-	bytes[5] = byte((c.Size & 0x000000FF))
-	return append(bytes, c.Data...)
+	bytes[1] = byte((c.ClientID >> 24) & 0xFF)
+	bytes[2] = byte((c.ClientID >> 16) & 0xFF)
+	bytes[3] = byte((c.ClientID >> 8) & 0xFF)
+	bytes[4] = byte(c.ClientID & 0xFF)
+	bytes[5] = byte(c.Type)
+	bytes[6] = byte(c.Format)
+	bytes[7] = byte((c.Size >> 24) & 0xFF)
+	bytes[8] = byte((c.Size >> 16) & 0xFF)
+	bytes[9] = byte((c.Size >> 8) & 0xFF)
+	bytes[10] = byte((c.Size & 0xFF))
+
+	var b net.Buffers = [][]byte{bytes, c.Data}
+
+	return b.WriteTo(conn)
 
 }
 
 func Unmarshal(d []byte) (*Command, error) {
-	s, t, e := ValidateHeader(d[:HeaderSize])
+	s, t, id, f, e := ValidateHeader(d[:HeaderSize])
 	if e != nil {
 		return nil, e
 	}
 	return &Command{
-		Version: CommandVersion(d[0]),
-		Type:    t,
-		Size:    s,
-		Data:    d[HeaderSize : int(s)+HeaderSize],
+		Version:  CommandVersion(d[0]),
+		ClientID: id,
+		Type:     t,
+		Format:   f,
+		Size:     s,
+		Data:     d[HeaderSize : int(s)+HeaderSize],
 	}, nil
 }
 
-func ValidateHeader(header []byte) (msgSize uint32, cmd CommandType, err error) {
+func ValidateHeader(header []byte) (msgSize uint32, cmd CommandType, id uint32, format uint8, err error) {
 	if len(header) < HeaderSize {
 		err = fmt.Errorf("malformed header")
 		return
@@ -69,8 +98,11 @@ func ValidateHeader(header []byte) (msgSize uint32, cmd CommandType, err error) 
 		return
 	}
 
-	msgSize = (uint32(header[2]) << 24) + (uint32(header[3]) << 16) + (uint32(header[4]) << 8) + (uint32(header[5]))
-	cmd = CommandType(header[1])
+	id = (uint32(header[1]) << 24) + (uint32(header[2]) << 16) + (uint32(header[3]) << 8) + uint32(header[4])
+	cmd = CommandType(header[5])
+	format = uint8(header[6])
+
+	msgSize = (uint32(header[7]) << 24) + (uint32(header[8]) << 16) + (uint32(header[9]) << 8) + (uint32(header[10]))
 
 	return
 }
